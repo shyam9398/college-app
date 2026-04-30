309ad052bc969295bfbdfa47b17cf424d00fd77b
Syamala rao <burlasyamalarao99@gmail.com>
2026-04-30 14:27:43 +0530

    updated db config

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,10 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'college_db',
-  password: 'syamalarao99',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
